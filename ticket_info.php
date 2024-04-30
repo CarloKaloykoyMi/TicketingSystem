90adb3f2ea97237cdfad8d4a28f0bc4413c65dbb
evagraceest <evalyngrace.estrera@my.jru.edu>
2024-04-30 17:38:49 +0800

    text area delete reason

--- ticket_info.php
+++ ticket_info.php
@@ -195,9 +195,9 @@ $reply_result = mysqli_query($con, $query);
                             </b>
                         </span>
                         <!-- Updated Status modal -->
-                        <button type="button" class="btn badge text-bg-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"  <?php if ($status == 'Cancelled' || $status == 'Resolved') {
-                                                                                                                                                                            echo 'disabled';
-                                                                                                                                                                        } ?>>Update Status
+                        <button type="button" class="btn badge text-bg-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" <?php if ($status == 'Cancelled' || $status == 'Resolved') {
+                                                                                                                                            echo 'disabled';
+                                                                                                                                        } ?>>Update Status
                         </button>
                         <br>
                         <?php
@@ -293,12 +293,13 @@ $reply_result = mysqli_query($con, $query);
                                                 <input type="hidden" name="ticket_id" value="<?php echo $ticket_id; ?>">
                                                 <input type="hidden" name="user_id" value="<?php echo $userid1; ?>">
                                                 Are you sure you want to delete this ticket?
+                                                <textarea class="form-control" name="delete_reason" rows="4" placeholder="Reason" required></textarea>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                                    <button type="submit" name="delete_ticket" class="btn btn-danger">Delete</button>
+                                                </div>
+                                            </form>
                                         </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                            <button type="submit" name="delete_ticket" class="btn btn-danger">Delete</button>
-                                        </div>
-                                        </form>
                                     </div>
                                 </div>
                             </div>
